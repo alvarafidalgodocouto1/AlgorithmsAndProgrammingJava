45ae7ab8d059d9940c17414c9d78849a7bfa67e7
alvarafidalgodocouto1 <alvarafidalgodocouto@gmail.com>
2025-11-21 18:43:33 +0000

    Add files via upload

---
+++ AlgorithmsAndProgrammingJava/scr/Basic/Loops.java
@@ -0,0 +1,13 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        // Repetition (The numbers are defined beforehand): for // Wiederholung: for
+
+        // Print numbers from 1 to 100 // Zahlen von 1 bis 100 ausgeben
+
+        // Option 1: declarar dentro do for
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
