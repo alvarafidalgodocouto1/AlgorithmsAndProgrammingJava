0fc842e51659e2c4664780c8bbe0007dd55bba55
alvarafidalgodocouto1 <alvarafidalgodocouto@gmail.com>
2025-11-21 17:14:38 +0000

    Delete AlgorithmsAndProgrammingJava/src directory

--- AlgorithmsAndProgrammingJava/src/Main.java
+++
@@ -1,9 +0,0 @@
-// Simple written exercise // Einfacher schriftlicher ubung
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Ich bin Álvara und komme aus Portugal. Danke und tschuss");
-        System.out.println("Hallo, koffee mit milch und kekse, bitte!");
-    }
-}
-
-//
