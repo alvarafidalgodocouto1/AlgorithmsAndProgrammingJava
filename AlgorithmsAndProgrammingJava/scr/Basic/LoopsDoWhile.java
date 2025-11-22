d4fb70b02b1678ccd5870c9e683695769ed19711
alvarafidalgodocouto1 <alvarafidalgodocouto@gmail.com>
2025-11-22 16:44:27 +0000

    Add files via upload

---
+++ AlgorithmsAndProgrammingJava/scr/Basic/LoopsDoWhile.java
@@ -0,0 +1,31 @@
+// Write number 1 to 10
+
+
+public class LoopsDoWhile {
+    public static void main(String[] args) {
+        // Variables
+
+        int i = 0;
+
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i <= 10);{
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
